4643f95a987eccb6c5b4809ed4a58c4a0c4e5005
patrieli <elias@patrikainen.fi>
2020-05-23 10:48:31 +0300

    added login_required for task paths

--- application/tasks/views.py
+++ application/tasks/views.py
@@ -1,17 +1,22 @@
-from application import app, db
 from flask import redirect, render_template, request, url_for
+from flask_login import login_required
+
+from application import app, db
 from application.tasks.models import Task
 from application.tasks.forms import TaskForm
 
 @app.route("/tasks", methods=["GET"])
+@login_required
 def tasks_index():
     return render_template("tasks/list.html", tasks= Task.query.all())
 
 @app.route("/tasks/create/")
+@login_required
 def tasks_form():
     return render_template("tasks/create.html", form = TaskForm())
 
 @app.route("/tasks/<task_id>/", methods=["POST"])
+@login_required
 def activate_task(task_id):
 
     t = Task.query.get(task_id)
@@ -22,6 +27,7 @@ def activate_task(task_id):
     return redirect(url_for("tasks_index"))
 
 @app.route("/tasks/complete/<task_id>/", methods=["POST"])
+@login_required
 def complete_task(task_id):
 
     t = Task.query.get(task_id)
@@ -32,8 +38,12 @@ def complete_task(task_id):
     return redirect(url_for("tasks_index"))
 
 @app.route("/tasks/", methods=["POST"])
+@login_required
 def create_tasks():
     form = TaskForm(request.form)
+
+    if not form.validate():
+        return render_template("tasks/create.html", form = form)
 
     t = Task(form.name.data, form.description.data)
 
@@ -43,6 +53,7 @@ def create_tasks():
     return redirect(url_for("tasks_index"))
 
 @app.route("/tasks/delete/<task_id>/", methods=["POST"])
+@login_required
 def delete_task(task_id):
 
     t = Task.query.get(task_id)
