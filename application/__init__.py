ee460b275b69f99744905c600d34c3c75e37e76a
patrieli <elias@patrikainen.fi>
2020-05-23 10:50:01 +0300

    added LoginManager and auth imports

--- application/__init__.py
+++ application/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
+
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 from flask_sqlalchemy import SQLAlchemy
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todoplanner.db"
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
@@ -12,5 +16,23 @@ from application import views
 
 from application.tasks import models
 from application.tasks import views
+
+from application.auth import models
+from application.auth import views
+
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Please login to use this functionality."
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 db.create_all()
