ac308d4c0feef92b239e46abbb77090e767d4383
patrieli <elias@patrikainen.fi>
2020-06-01 20:41:53 +0300

    deadline and labels added

--- application/tasks/forms.py
+++ application/tasks/forms.py
@@ -1,9 +1,12 @@
+from datetime import datetime, date
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators
+from wtforms import StringField, DateField, validators
 
 class TaskForm(FlaskForm):
     name = StringField("Task name", [validators.Length(min=3)])
     description = StringField("Description")
+    deadline = DateField('Deadline', format='%d/%m/%Y')
+    labels = StringField("Labels")
  
     class Meta:
         csrf = False
