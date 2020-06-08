7dcf4f27f1e6babdaa805fa897c56c4e2e8d183a
patrieli <elias@patrikainen.fi>
2020-06-08 23:07:05 +0300

    validators updated

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -3,11 +3,11 @@ from wtforms import PasswordField, StringField, validators
   
 class LoginForm(FlaskForm):
     username = StringField('Username', [validators.Length(min=3, max=30), validators.Regexp('[0-9A-Za-z_]+'), validators.DataRequired()])
-    password = PasswordField('Password', [validators.DataRequired()])
+    password = PasswordField('Password', [validators.Length(min=3, max=144), validators.DataRequired()])
   
     class Meta:
         csrf = False
 
 class UserForm(FlaskForm):
     username = StringField('Username', [validators.Length(min=3, max=30), validators.Regexp('[0-9A-Za-z_]+'), validators.DataRequired()])
-    password = PasswordField('Password', [validators.DataRequired()])
+    password = PasswordField('Password', [validators.Length(min=3, max=144), validators.DataRequired()])
--- application/tasks/forms.py
+++ application/tasks/forms.py
@@ -3,8 +3,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, validators
 
 class TaskForm(FlaskForm):
-    name = StringField("Task name", [validators.Length(min=3)])
-    description = StringField("Description", [validators.DataRequired()])
+    name = StringField("Task name", [validators.Length(min=3, max=30)])
+    description = StringField("Description", [validators.Length(max=144)])
     deadline = DateField('Deadline', format='%d/%m/%Y')
     labels = StringField("Labels")
  
