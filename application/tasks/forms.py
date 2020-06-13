d7faba2ee473eb40fecfd574d71040e25a7be69c
patrieli <elias@patrikainen.fi>
2020-06-13 23:58:45 +0300

    validators changed

--- application/tasks/forms.py
+++ application/tasks/forms.py
@@ -3,7 +3,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, validators
 
 class TaskForm(FlaskForm):
-    name = StringField("Task name", [validators.Length(min=3, max=30)])
+    name = StringField("Task name", [validators.Length(min=3, max=30), validators.data_required()])
     description = StringField("Description", [validators.Length(max=144)])
     deadline = DateField('Deadline', format='%d/%m/%Y')
     labels = StringField("Labels")
