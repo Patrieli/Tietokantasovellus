6fe125be33bfff3a915a5ff3c4da48caea113dc8
patrieli <elias@patrikainen.fi>
2020-06-18 21:32:34 +0300

    added EditForm

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, validators
+from wtforms import PasswordField, StringField, SelectField, validators
   
 class LoginForm(FlaskForm):
     username = StringField('Username', [validators.Length(min=3, max=30), validators.Regexp('[0-9A-Za-z_]+'), validators.DataRequired()])
@@ -13,5 +13,12 @@ class UserForm(FlaskForm):
     password = PasswordField('Password', [validators.Length(min=3, max=144), validators.DataRequired()])
     repassword = PasswordField('Confirm password', [validators.Length(min=3, max=144), validators.DataRequired()])
 
+    class Meta:
+        csrf = False
+
+class EditForm(FlaskForm):
+    username = StringField('Username',  [validators.Length(min=3, max=30), validators.Regexp('[0-9A-Za-z_]+'), validators.DataRequired()])
+    role = SelectField('Role', choices=[('USER', 'User'), ('ADMIN', 'Admin')])
+    
     class Meta:
         csrf = False
