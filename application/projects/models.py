ccf0c9a7548c1d49579eb759d45692ea4d1f671f
patrieli <elias@patrikainen.fi>
2020-06-19 12:39:35 +0300

    project_tasks query updated

--- application/projects/models.py
+++ application/projects/models.py
@@ -18,7 +18,7 @@ class Project(Base):
     @staticmethod
     def project_tasks(project_id):
 
-        stmt = text("SELECT task.id, task.name, task.deadline, task.state, COUNT(task.id) AS count FROM project"
+        stmt = text("SELECT task.id, task.name, task.deadline, task.state, task.description, COUNT(task.id) AS count FROM project"
                     " INNER JOIN task ON (task.project_id = project.id)"
                     " WHERE (task.project_id = :project_id) "
                     " GROUP BY task.id, task.name, task.deadline, task.state" ).params(project_id=project_id)
@@ -27,6 +27,6 @@ class Project(Base):
 
         response = []
         for row in res:
-            response.append({"id":row[0], "name":row[1], "deadline":row[2], "state":row[3], "count":row[4]})
+            response.append({"id":row[0], "name":row[1], "deadline":row[2], "state":row[3], "description":row[4], "count":row[5]})
 
         return response
